7265b4f078f41db37a64ba277b6954c5b764a3e6
ckogstrom <32303851+ckogstrom@users.noreply.github.com>
2019-09-18 17:27:24 -0400

    mistakenly pushed

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    db: {
-      uri: 'mongodb+srv://ckogstrom:9msh2mXty3tVarRq@cluster0-ig4hj.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    }
-  };

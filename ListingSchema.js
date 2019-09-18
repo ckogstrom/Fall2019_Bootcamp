a30bba6ac64992eb1a8fa7da82f1c2853e002d6b
Carl Kogstrom <ckogstrom@ufl.edu>
2019-09-18 13:27:23 -0400

    added created_at

--- ListingSchema.js
+++ ListingSchema.js
@@ -13,6 +13,8 @@ var listingSchema = new Schema({
   name: {type: String, required: true},
   coordinates:{latitude:Number, longitude:Number},
   address: String
+  created_at: Date,
+  updated_at: Date,
 });
 
 /* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
